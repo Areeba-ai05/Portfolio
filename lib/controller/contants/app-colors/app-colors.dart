b76e8ad88d7827e9dd92f774932c0585235d0a1a
Areeba Iqbal <areeba.seo05@gmail.com>
2025-01-08 22:09:51 +0500

    add files

---
+++ lib/controller/contants/app-colors/app-colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class AppColors{
+  static Color  primaryColor=Color(0xFFFFFF);
+  static Color  primaryColor2=Color(0x457B9D);
+  static Color  backgroundColor=Color(0x457B9D);
+  static Color  secondaryColor=Color(0xFFA8A7);
+  static Color  blackColor=Colors.black;
+  static Color  whiteColor=Colors.white;
+  static Color  secondaryColor2=Color(0xF28F8F);
+  static Color  zincColor=Color(0x263238);
+  static Color  tealColor=Color(0x455A64);
+  static Color  peachColor=Color(0xFFA8A7);
+  static Color  darkpeachColor=Color(0xF28F8F);
+
+}
